efedd3d5cd12c407462e3b8bc15b334dbbbf5ad8
guoliangz588 <guoliangzheng@redshore.ai>
2025-10-23 07:59:11 +0800

    edit next cofig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 忽略 TypeScript 构建错误
+  typescript: {
+    ignoreBuildErrors: true, // [!code ++]
+  },
+  // 忽略 ESLint 构建错误
+  eslint: {
+    ignoreDuringBuilds: true, // [!code ++]
+  },
+}
 
-export default nextConfig
+module.exports = nextConfig
